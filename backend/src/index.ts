93b799e2bdf8629bf1ae196b7b008a863a6a6af1
dampuero <ampuero.diegob@gmail.com>
2024-07-08 17:18:07 -0400

    MOD: src/index.ts
    Se importaron las rutas de routes/pedidos y routes/productos

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,14 @@ import comunaRoutes from './routes/comunaRoutes';
 import regionRoutes from './routes/regionRoutes';
 import roles from './routes/rolesRoutes';
 import rolUsuario from './routes/rolUsuarioRoutes';
+import detallePedidoRoutes from './routes/pedidos/detallePedidoRoutes';
+import pedidosRoutes from './routes/pedidos/pedidosRoutes';
+import proveedorRoutes from './routes/pedidos/proveedorRoutes';
+import bodegasRoutes from './routes/productos/bodegasRoutes';
+import categoriaRoutes from './routes/productos/categoriaRoutes';
+import devolucionesRoutes from './routes/productos/devolucionesRoutes';
+import productosRoutes from './routes/productos/productosRoutes';
+import registroPreciosRoutes from './routes/productos/registroPreciosRoutes';
 import { AppDataSource } from './config/data-source';
 
 dotenv.config();
@@ -24,6 +32,14 @@ app.use('/api', comunaRoutes);
 app.use('/api', regionRoutes);
 app.use('/api', roles);
 app.use('/api', rolUsuario);
+app.use('/api', detallePedidoRoutes);
+app.use('/api', pedidosRoutes);
+app.use('/api', proveedorRoutes);
+app.use('/api', bodegasRoutes);
+app.use('/api', categoriaRoutes);
+app.use('/api', devolucionesRoutes);
+app.use('/api', productosRoutes);
+app.use('/api', registroPreciosRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
